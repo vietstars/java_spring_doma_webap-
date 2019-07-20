51b8e4651d9f7bcc87028f072067e841981196e5
vietstars <vietstar.nt@gmail.com>
2019-07-20 22:58:11 +0700

    jwt 0.9.1

--- src/main/java/pwater/controller/Home.java
+++ src/main/java/pwater/controller/Home.java
@@ -7,14 +7,42 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.crypto.MacProvider;
+import java.security.Key;
+
 import Pwater.Model.Account;
 
 @RestController
 public class Home {
 
-    @RequestMapping(value = {"/","/Home","/home"}, method = RequestMethod.GET)
+	
+	public Home(){
+		Key key = MacProvider.generateKey();
+		String compactJws = Jwts.builder()
+		  .setSubject("Joe")
+		  .signWith(SignatureAlgorithm.HS512, key)
+		  .compact();
+		System.out.println(key.toString());
+		System.out.println(compactJws);
+		//unitTest: assert Jwts.parser().setSigningKey(key).parseClaimsJws(compactJws).getBody().getSubject().equals("Joe");
+		System.out.println(Jwts.parser().setSigningKey(key).parseClaimsJws(compactJws));
+	}
+
+    @RequestMapping(value = {"/","/Home","/home"}, method = RequestMethod.GET, produces = "application/json")
     public List index() {
     	List account = new Account().GetAll();
+  //   	try {
+
+		//     Jwts.parser().setSigningKey(key).parseClaimsJws(compactJws);
+
+		//     //OK, we can trust this JWT
+
+		// } catch (SignatureException e) {
+
+		//     //don't trust the JWT!
+		// }
     	return account;
     }
 
